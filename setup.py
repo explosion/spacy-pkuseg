06e32b2b36e3394df2ffb1dda4afe65ecdf61240
Ines Montani <ines@ines.io>
2020-10-07 11:31:01 +0200

    Tidy up and add setup.cfg

--- setup.py
+++ setup.py
@@ -9,10 +9,6 @@ def is_source_release(path):
 
 def setup_package():
     root = os.path.abspath(os.path.dirname(__file__))
-
-    long_description = "file: README.md"
-    long_description_content_type = "text/markdown"
-
     extensions = [
         Extension(
             "spacy_pkuseg.inference",
@@ -31,7 +27,7 @@ def setup_package():
             include_dirs=[np.get_include()],
         ),
     ]
-    
+
     if not is_source_release(root):
         from Cython.Build import cythonize
         extensions = cythonize(extensions, annotate=True)
@@ -39,24 +35,9 @@ def setup_package():
 
     setuptools.setup(
         name="spacy_pkuseg",
-        version="0.0.27",
-        author="Explosion",
-        author_email="contact@explosion.ai",
-        description="A small package for Chinese word segmentation",
-        long_description=long_description,
-        long_description_content_type=long_description_content_type,
-        url="https://github.com/explosion/spacy-pkuseg",
         packages=setuptools.find_packages(),
         package_data={"": ["*.txt*", "*.pkl", "*.npz", "*.pyx", "*.pxd"]},
-        classifiers=[
-            "Programming Language :: Python :: 3",
-            "License :: Other/Proprietary License",
-            "Operating System :: OS Independent",
-        ],
-        install_requires=["cython", "numpy>=1.16.0", "srsly>=2.3.0,<3.0.0"],
-        setup_requires=["cython", "numpy>=1.16.0"],
         ext_modules=extensions,
-        zip_safe=False,
     )
 
 
