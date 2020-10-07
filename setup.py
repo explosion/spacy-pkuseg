3716fc7229c85ad8a140babbdd712050597d8ccc
Adriane Boyd <adrianeboyd@gmail.com>
2020-10-07 10:33:02 +0200

    Add LICENSE and README.md to MANIFEST.in

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def setup_package():
 
     setuptools.setup(
         name="spacy_pkuseg",
-        version="0.0.26",
+        version="0.0.27",
         author="Explosion",
         author_email="contact@explosion.ai",
         description="A small package for Chinese word segmentation",
